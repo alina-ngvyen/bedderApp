53b0db94af18cda8572dde9864c865ab3b0f8a32
jennaduong <jennavy30@gmail.com>
2022-06-15 10:02:09 -0500

    I did the importantOfSleep through betterSleepTips

--- bedderApp/LearnViewController.swift
+++ bedderApp/LearnViewController.swift
@@ -6,16 +6,31 @@
 //
 
 import UIKit
+import WebKit
 
 class LearnViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
+    @IBAction func importanceOfSleep(_ sender: Any) {
+        let googleURL = URL(string: "https://sph.umich.edu/pursuit/2020posts/why-sleep-is-so-important-to-your-health.html")
+                            UIApplication.shared.open(googleURL!)
+    }
+    @IBAction func fallAsleepFaster(_ sender: Any) {
+        let googleURL = URL(string: "https://www.healthline.com/nutrition/ways-to-fall-asleep#3.-Get-on-a-schedule")
+        UIApplication.shared.open(googleURL!)
+    }
+    @IBAction func betterSleepTips(_ sender: Any) {
+        let googleURL = URL(string: "https://www.healthline.com/nutrition/17-tips-to-sleep-better#Food-Fix:-Foods-for-Better-Sleep")
+        UIApplication.shared.open(googleURL!)
+    }
     
-
+    
+    
+    
     /*
     // MARK: - Navigation
 

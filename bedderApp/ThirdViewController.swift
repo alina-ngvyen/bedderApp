84ba36c719928922243b4e42d6bce4cd0fbd10fd
Alina Nguyen <alinangvyen@gmail.com>
2022-06-16 09:06:06 -0500

    ...

--- bedderApp/ThirdViewController.swift
+++ bedderApp/ThirdViewController.swift
@@ -11,6 +11,7 @@ class ThirdViewController: UIViewController {
 
     @IBOutlet weak var nameInput: UITextField!
     @IBOutlet weak var ageInput: UITextField!
+    @IBOutlet weak var sleepRec: UILabel!
     
     @IBAction func nextButton(_ sender: Any)
     {
@@ -24,6 +25,15 @@ class ThirdViewController: UIViewController {
         fourthController.userName = nameInput.text!
         }
 
+    @IBAction func ageEntered(_ sender: Any) {
+        var sleepH = "7-9"
+        if ageInput >= 65{
+            sleepH = "7-8"
+            else if ageInput "
+        }
+        
+        sleepRec.text = "did you know that you should be sleeping \(sleepH) hours each night?"    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
